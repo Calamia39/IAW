0f2111180b980b032942010d5f1070db1019714c
CJ\djartel <damijaraterll@gmail.com>
2023-10-03 12:25:11 +0200

    Subido ejercicios de condicionales

---
+++ EjerciciosCondicionales/EJ-2.py
@@ -0,0 +1,7 @@
+Valor1 = int(input("Dame el primer valor :"))
+Valor2 = int(input("Dame el segundo valor :"))
+
+if (Valor1>Valor2):
+    print("Valor2 es menor")
+else:
+    print("valor1 es menor")    
