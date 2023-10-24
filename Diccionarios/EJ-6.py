d14c3736c633addca75f880f15e301736f9c15e0
CJ\djartel <damijaraterll@gmail.com>
2023-10-24 10:20:41 +0200

    subiendo diccionario

---
+++ Diccionarios/EJ-6.py
@@ -0,0 +1,17 @@
+diccionario={}
+nombre = input("Escriba su nombre: ")
+diccionario["Nombre"]=nombre
+print(diccionario)
+edad =int( input("Escriba su edad: "))
+diccionario["Edad"]=edad
+print(diccionario)
+sexo = input("Escriba su sexo: ")
+diccionario["Sexo"]=sexo
+print(diccionario)
+tel =int( input("Escriba su telefono: "))
+diccionario["Telefono"]=tel
+print(diccionario)
+correo =input("Escriba su Correo electronico: ")
+diccionario["Correo"]=correo
+print(diccionario)
+    
